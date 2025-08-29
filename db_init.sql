886575abaf75b19a00f77181a56a68b8eea84bc3
mahmoudsaad213 <60885213+mahmoudsaad213@users.noreply.github.com>
2025-08-29 20:31:53 +0300

    Update db_init.sql

--- db_init.sql
+++ db_init.sql
@@ -1,1 +1,10 @@
 
+CREATE TABLE payments (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  nonce TEXT,
+  customer_id TEXT,
+  payment_method_token TEXT,
+  transaction_id TEXT,
+  subscription_id TEXT,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
